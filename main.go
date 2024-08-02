3e0f150adf1ae827b8a402d63a2f4cc159e8a314
Hayden Duffy <45819703+duffpop@users.noreply.github.com>
2024-08-02 20:03:12 +0100

    broken img conversion

--- main.go
+++ main.go
@@ -1,9 +1,13 @@
 package main
 
 import (
+	"bytes"
+	"errors"
+	"fmt"
+	"image/jpeg"
+	"image/png"
 	"net/http"
 	"net/url"
-	"path/filepath"
 
 	"github.com/gin-gonic/gin"
 )
@@ -25,11 +29,17 @@ func main() {
 			return
 		}
 
-		filename := filepath.Base(file.Filename)
-		if err := c.SaveUploadedFile(file, filename); err != nil {
-			c.String(http.StatusBadRequest, "upload file err: %s", err.Error())
+		// Convert to png
+		fileBytes, err := file.Open()
+		if err != nil {
+			c.String(http.StatusBadRequest, "open file err: %s", err.Error())
 			return
 		}
+		defer fileBytes.Close()
+
+		imgBytes := new(bytes.Buffer)
+		imgBytes.ReadFrom(fileBytes)
+		imgBytes, err = imgToPng(imgBytes.Bytes(), err)
 
 		urlGen := url.URL{
 			Scheme: "https",
@@ -37,8 +47,50 @@ func main() {
 			Path:   "/" + filename,
 		}
 
-		c.String(http.StatusOK, "SUCCESS: %s/%s", urlGen, file.Filename)
+		c.String(http.StatusOK, "SUCCESS: %s/%s", urlGen)
 	})
 
 	router.Run(":8080")
+}
+
+// imgToPng converts an image to png
+func imgToPng(imageBytes []byte, err error) ([]byte, error) {
+	contentType := http.DetectContentType(imageBytes)
+
+	switch contentType {
+	case "image/png":
+	case "image/jpeg":
+		img, err := jpeg.Decode(bytes.NewReader(imageBytes))
+		if err != nil {
+			return nil, errors.Join(err, errors.New("unable to decode jpeg"))
+		}
+
+		buf := new(bytes.Buffer)
+		if err := png.Encode(buf, img); err != nil {
+			return nil, errors.Join(err, errors.New("unable to encode png"))
+		}
+
+		return buf.Bytes(), nil
+	}
+
+	return nil, fmt.Errorf("unable to convert %#v to png", contentType)
+}
+
+// gets image from multipart form upload
+func getImageFromForm(c *gin.Context) ([]byte, error) {
+	file, err := c.FormFile("file")
+	if err != nil {
+		return nil, err
+	}
+
+	fileBytes, err := file.Open()
+	if err != nil {
+		return nil, err
+	}
+	defer fileBytes.Close()
+
+	buf := new(bytes.Buffer)
+	buf.ReadFrom(fileBytes)
+
+	return buf.Bytes(), nil
 }
