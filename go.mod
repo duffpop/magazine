c71880315f2a70a97d497ec453db41e3743fa13a
Hayden Duffy <45819703+duffpop@users.noreply.github.com>
2024-08-04 08:50:30 +0100

    random namegen

--- go.mod
+++ go.mod
@@ -27,11 +27,12 @@ require (
 	github.com/twitchyliquid64/golang-asm v0.15.1 // indirect
 	github.com/ugorji/go/codec v1.2.12 // indirect
 	golang.org/x/arch v0.8.0 // indirect
-	golang.org/x/crypto v0.23.0 // indirect
+	golang.org/x/crypto v0.24.0 // indirect
 	golang.org/x/image v0.18.0
-	golang.org/x/net v0.25.0 // indirect
-	golang.org/x/sys v0.20.0 // indirect
+	golang.org/x/net v0.26.0 // indirect
+	golang.org/x/sys v0.21.0 // indirect
 	golang.org/x/text v0.16.0 // indirect
 	google.golang.org/protobuf v1.34.1 // indirect
 	gopkg.in/yaml.v3 v3.0.1 // indirect
+	tailscale.com v1.70.0
 )
--- main.go
+++ main.go
@@ -4,14 +4,17 @@ import (
 	"bytes"
 	"errors"
 	"image"
+	"image/jpeg"
 	"image/png"
 	"io"
+	"math/rand"
 	"net/http"
 	"net/url"
 	"path/filepath"
 
 	"github.com/chai2010/webp"
 	"github.com/gin-gonic/gin"
+	"tailscale.com/words"
 )
 
 var host string = "img.hayden.lol"
@@ -22,48 +25,51 @@ func main() {
 	// Set a lower memory limit for multipart forms (default is 32 MiB)
 	router.MaxMultipartMemory = 8 << 20 // 8 MiB
 
-	// router.Static("/", "./public")
 	router.POST("/upload", func(c *gin.Context) {
 		// Source
 		file, err := c.FormFile("file")
 		if err != nil {
-			c.String(http.StatusBadRequest, "get form err: %s", err.Error())
+			c.String(http.StatusBadRequest, "ERR: get form err: %s", err.Error())
 			return
 		}
 
 		filename := filepath.Base(file.Filename)
-		if err := c.SaveUploadedFile(file, filename); err != nil {
-			c.String(http.StatusBadRequest, "upload file err: %s", err.Error())
+		ext := filepath.Ext(filename)
+
+		// Open the file
+		imageBytes, err := file.Open()
+		if err != nil {
+			c.String(http.StatusBadRequest, "ERR: unable to open file: %s", err.Error())
+			return
+		}
+		defer imageBytes.Close()
+
+		// Read the file content
+		fileContent, err := io.ReadAll(imageBytes)
+		if err != nil {
+			c.String(http.StatusBadRequest, "ERR: unable to read file: %s", err.Error())
 			return
 		}
 
-		// check syntax of the filename, it should be a webp file
-		// if not, convert to webp
-		if filepath.Ext(filename) != ".webp" {
+		if ext != ".webp" {
 			// Convert to webp
-			imageBytes, err := file.Open()
-			if err != nil {
-				c.String(http.StatusBadRequest, "unable to open file: %s", err.Error())
-				return
-			}
-			defer imageBytes.Close()
-
-			// Read the file content
-			fileContent, err := io.ReadAll(imageBytes)
-			if err != nil {
-				c.String(http.StatusBadRequest, "unable to read file: %s", err.Error())
-				return
-			}
-
 			img, err := convertToWebP(fileContent)
 			if err != nil {
-				c.String(http.StatusBadRequest, "unable to convert to webp: %s", err.Error())
+				c.String(http.StatusBadRequest, "ERR: unable to convert to webp: %s", err.Error())
 				return
 			}
 
 			// Save the webp file
-			if err := webp.Save(filename, img, &webp.Options{Lossless: true}); err != nil {
-				c.String(http.StatusBadRequest, "unable to save webp: %s", err.Error())
+			filename = filename[:len(filename)-len(ext)] + ".webp"
+			newFilename := wordGen() + ".webp"
+			if err := webp.Save(newFilename, img, &webp.Options{Lossless: true}); err != nil {
+				c.String(http.StatusBadRequest, "ERR: unable to save webp: %s", err.Error())
+				return
+			}
+		} else {
+			// Save the original webp file
+			if err := c.SaveUploadedFile(file, filename); err != nil {
+				c.String(http.StatusBadRequest, "ERR: upload file err: %s", err.Error())
 				return
 			}
 		}
@@ -74,7 +80,7 @@ func main() {
 			Path:   "/" + filename,
 		}
 
-		c.String(http.StatusOK, "SUCCESS: %s/%s", urlGen, file.Filename)
+		c.String(http.StatusOK, "SUCCESS: %s", urlGen.String())
 	})
 
 	router.Run(":8080")
@@ -84,8 +90,6 @@ func convertToWebP(imageBytes []byte) (image.Image, error) {
 	var buf bytes.Buffer
 	contentType := http.DetectContentType(imageBytes)
 	imgReader := bytes.NewReader(imageBytes)
-	// strippedName := filepath.Base(name)
-	// convertedName := strippedName[0 : len(strippedName)-len(filepath.Ext(strippedName))] // Remove file extension
 
 	switch contentType {
 	case "image/png":
@@ -99,6 +103,36 @@ func convertToWebP(imageBytes []byte) (image.Image, error) {
 		}
 
 		return img, nil
+	case "image/jpeg":
+		img, err := jpeg.Decode(imgReader)
+		if err != nil {
+			return nil, errors.Join(err, errors.New("unable to decode jpeg"))
+		}
+
+		if err = webp.Encode(&buf, img, &webp.Options{Lossless: true}); err != nil {
+			return nil, errors.Join(err, errors.New("unable to encode webp"))
+		}
+
+		return img, nil
 	}
-	return nil, nil
+	return nil, errors.New("unsupported image format")
+}
+
+func wordGen() string {
+	scales := words.Scales()
+	tails := words.Tails()
+
+	var word string
+
+	for i := 0; i < 1; i++ {
+		word += scales[rand.Intn(len(scales))]
+	}
+
+	word += "-"
+
+	for i := 0; i < 1; i++ {
+		word += tails[rand.Intn(len(scales))]
+	}
+
+	return word
 }
